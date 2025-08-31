178ef083d4f9b4e6ebde53e728de760abc1c6bd0
Dat Nguyen <ddatnguyen97@gmail.com>
2025-09-01 01:07:35 +0700

    Add event listener for date picker slicer

--- dashboard/assets/events.js
+++ dashboard/assets/events.js
@@ -13,15 +13,12 @@ function pushDateFilterEvent(filterValue) {
   });
 }
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const datePicker = document.getElementById("global-date-picker");
+document.addEventListener("DOMContentLoaded", function () {
+  const datePicker = document.getElementById("global-date-picker");
 
-//   if (container) {
-//     const input = container.querySelector("input");
-//     if (input) {
-//       input.addEventListener("change", function () {
-//         pushDateFilterEvent(input.value);
-//       });
-//     }
-//   }
-// });
+  if (datePicker) {
+    datePicker.addEventListener("change", function () {
+      pushDateFilterEvent(datePicker.value);
+    });
+  }
+});
